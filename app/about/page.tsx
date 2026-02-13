47bc37501809f1ad86968dbb9d6392f991e2e98f
Anurag <anurag8731@gmail.com>
2026-02-13 12:16:37 +0530

    Day 1 - Next.js setup and basic form UI

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return(
+        <div className="p-10">
+            <h2 className="text-2xl font-bold">About Page</h2>
+        </div>
+    )
+}
---
+++ app/contact/page.jsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+    return(
+        <div className="p-10">
+            <h2 className="text-2xl font-bold">Contact Page</h2>
+        </div>
+    )
+}
