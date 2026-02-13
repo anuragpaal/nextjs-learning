7854b6a9bca32e9f15345ff4fa13658c3e118789
Anurag <anurag8731@gmail.com>
2026-02-13 14:23:17 +0530

    DAY 7 — Search & Filter

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,42 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [posts, setPosts] = useState<any[]>([]);
+  const [search, setSearch] = useState("");
 
-  const router = useRouter();
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data.slice(0, 10)));
+  }, []);
+
+  const filteredPosts = posts.filter((post) =>
+    post.title.toLowerCase().includes(search.toLowerCase()),
+  );
 
   return (
-    <main className="flex flex-col items-center justify-center h-screen gap-4">
-      <h1 className="text-3xl font-bold">
-        Day 6 — useRouter 🚀
-      </h1>
+    <div className="p-10">
+      <h1 className="text-3xl font-bold mb-4">Search Posts 🔍</h1>
 
-      <button
-        onClick={() => router.push("/posts")}
-        className="bg-blue-500 text-white px-4 py-2 rounded"
-      >
-        Go to Posts
-      </button>
+      <input
+        type="text"
+        placeholder="Search posts..."
+        className="border p-2 mb-4 w-full"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
 
-       <button
-        onClick={() => router.push("/contact?source=home")}
-        className="bg-blue-500 text-white px-4 py-2 rounded"
-      >
-        Go to Contact
-      </button>
-    </main>
+      {filteredPosts.length === 0 ? (
+        <p>No data found</p>
+      ) : (
+        filteredPosts.map((post) => (
+          <p key={post.id} className="mb-2">
+            {post.title}
+          </p>
+        ))
+      )}
+    </div>
   );
 }
